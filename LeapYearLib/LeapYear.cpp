ad8ac811848ab20e3b4cc84fdf892e23e052b9ed
manhqb877 <hoangducmanh877@gmail.com>
2025-11-08 08:40:08 +0700

    update test

--- LeapYearLib/LeapYear.cpp
+++ LeapYearLib/LeapYear.cpp
@@ -1,15 +1,16 @@
 
-bool IsLeapYear(int n)
-{
-    return (n % 4 == 0 && n % 100 != 0) || (n % 400 == 0);
-}
-
 //bool IsLeapYear(int n)
 //{
-//    if (n % 4 == 0 || (n % 100 == 0 && n % 400 == 0))
-//    {
-//        return true;
-//    }
-//    return false;
+//    return (n % 4 == 0 && n % 100 != 0) || (n % 400 == 0);
 //}
+
+bool IsLeapYear(int n)
+{
+    if (n % 4 == 0 || (n % 100 == 0 && n % 400 == 0))
+    {
+        return true;
+    }
+    return false;
+}
+
 
